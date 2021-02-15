bedd978d180d461c8b79227761a4c1108032986c
Rin <67466959+weirdsir@users.noreply.github.com>
2021-02-15 12:49:42 +0200

    Вывод результата

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
+<<<<<<< Updated upstream
 user = input("Введите букву r(ock),p(aper) или s(cissors)")
+=======
+import random
+
+letters = ["r", "p", "s"]
+exit = "q"
+
+while True:
+    user = input("Введите букву r(ock),p(aper) или s(cissors); Нажмите q для выхода.")
+    resp = random.choice(letters)
+    if user == exit:
+        break
+    elif user == resp:
+        print("ничья")
+    elif user == "r" and resp == "s":
+        print("победа")
+    elif user == "p" and resp == "r":
+        print("победа")
+    elif user == "s" and resp == "p":
+        print("победа")
+    else:
+        print("поражение")
+>>>>>>> Stashed changes
